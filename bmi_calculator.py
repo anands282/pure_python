3eab468615c9441d8019b31be59714ab9f6583ef
Anand Satheesh <anands282@gmail.com>
2024-06-27 10:23:37 +0530

    updated life in weeks

---
+++ bmi_calculator.py
@@ -0,0 +1,5 @@
+height = input("Enter the height of the person in meters: ")
+weight = input("Enter the weight in Kilograms: ")
+
+bmi = int(float(weight)/(float(height) ** 2))
+print(bmi)
---
+++ day1_print.py
@@ -0,0 +1,1 @@
+print("Hi This repo will contain code snipptes for python practice")
